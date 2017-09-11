fdc5a65a966c5d019c972d8cfa245352d8adb3b2
Anupam Gupta <anupam.gupta@aol.com>
2017-09-10 22:47:54 -0500

    Super Reduced String
    Super Reduced String

---
+++ 03 Strings/01 Super Reduced String.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String super_reduced_string(String s){
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i=1; i<sb.length();i++){
+            if(sb.charAt(i) == sb.charAt(i-1)){
+                sb.delete(i-1,i+1);
+                i=0;
+            }
+        }
+        
+        if(sb.length() == 0)
+            sb.append("Empty String");
+        
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String result = super_reduced_string(s);
+        System.out.println(result);
+    }
+}
